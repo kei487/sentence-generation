5fe1c9892737ac8fc09d2db05c6082b310f62150
Keitaro Nakamura <keitaro.nakamura@hoge.com>
2023-11-14 08:33:47 +0900

    update prog6.c

--- prog6.c
+++ prog6.c
@@ -3,84 +3,95 @@
 #include <time.h>
 #include <unistd.h>
 
-#define DATA_NUM 1200
+#define DATA_NUM 760
 
 struct let_data
 {
-    int rank;
     long int freq;
-    double prob;
     int id[2];
-    char let[8];
 };
 struct let_list
 {
-    int letter[10000];
     long int mam;
+    int letter[22223];
 };
 
 int print_char(int n);
 
 
 int main(){
+    puts("non statement error");
     FILE *fp;
-    char c1[10],c2[10],c3[10],c4[10],c5[10];
-    int i,j,max_let,tmp,a1,a0,k[130]={0},atmp[130]={0};
+    char c1[10];
+    int i,j,max_let,tmp,a1,a0,k[42]={0},atmp[42]={0};
     long int mam=0;
-    struct let_list list[130];
+    struct let_list list[42];
     struct let_data data[DATA_NUM];
+    char let[8];double prob;int rank;
 
-    for(i=0;i<130;i++){
+    for(i=0;i<42;i++){
         list[i].mam=0;
     }
     
     srand((unsigned int)time(NULL)); 
 
-    if((fp=fopen("rslprog3","r")) == NULL){
-		fprintf(stderr, "Can not open txte file\n");
+    if((fp=fopen("rslprog3n","r")) == NULL){
+		printf("Can not open txte file\n");
 		return -1;
 	}
-    
-    fscanf(fp,"%s,%s,%s,%s,%s",c1,c2,c3,c4,c5);
+
+    fscanf(fp,"%s,%s,%s,%s,%s",c1,c1,c1,c1,c1);
     i=0;
     do{
-        fscanf(fp,"%d,%ld,%lf,%d %d,%s",&data[i].rank,&data[i].freq,&data[i].prob,&data[i].id[0],&data[i].id[1],data[i].let);
-        mam+=data[i].freq;
-        list[data[i].id[0]].mam+=data[i].freq;
+        fscanf(fp,"%d,%ld,%lf,%d %d,%s",&rank,&data[i].freq,&prob,&data[i].id[0],&data[i].id[1],let);
     }while((data[i++].freq!=0)&&(i<DATA_NUM)); 
     max_let=i;
+
     
-    //この下がやばそう
     for(i=0;i<max_let;i++){
-        for( ;k[data[i].id[0]]<atmp[data[i].id[0]]+data[i].freq;k[data[i].id[0]]++){
-            list[data[i].id[0]].letter[k[data[i].id[0]]]=data[i].id[1];
+        for(j=0;j<42;j++){
+            //printf("%d::%d:%ld\n",i,j,list[j].mam);
         }
+        if(data[i].id[0]==2){
+            //printf("%d:%ld %ld %d\n",i,list[2].mam,data[i].freq,k[data[i].id[0]]);
+        }
+        for( ;k[data[i].id[0]]<atmp[data[i].id[0]]+data[i].freq/10; k[data[i].id[0]]++){
+            list[data[i].id[0]].letter[k[data[i].id[0]]]=data[i].id[1];
+            //if(i==3) printf("%d:%ld %ld %d\n",i,list[1].mam,data[i].freq,k[data[i].id[0]]);
+        }
+        list[data[i].id[0]].mam+=data[i].freq/10;
         atmp[data[i].id[0]]=k[data[i].id[0]];
-    }
+    }printf("sum:%ld\n",list[2].mam);
 
-    
-    int loop=rand()%mam;
-    printf("%d %ld\n",loop,mam);
-    for(i=0;i<130;i++){
-        
+    for(i=0;i<42;i++){
+        mam+=list[i].mam;
+        //printf("%ld\n",list[i].mam);
     }
-
+    printf("%ld\n",mam);
+    //ここ以下
+    int loop=rand()%mam;printf("%ld %d\n",mam,loop);
+    int flag=0;
     j=0;tmp=0;
-    for(i=0;i<130;i++){
+    for(i=0;i<121;i++){
+        printf("i");
         for( ;j<tmp+list[i].mam;j++){
             if(j==loop){
-                print_char(data[j].id[0]);
-                print_char(data[j].id[1]);
+                printf("%d,%d\n",i,list[i].letter[j-tmp]);
+                print_char(i);
+                print_char(list[i].letter[j-tmp]);
+                a0=list[i].letter[j-tmp];
+                flag=1;
                 break;
             }
         }
+        if(flag) break;
         tmp=j;
     }
     for(i=0;i<10000;i++){
-        a1=rand()%list[a0].mam;
+        a1 = rand() % list[a0].mam;
         print_char(list[a0].letter[a1]);
-        a0=a1;
-    }puts(" ");
+        a0=list[a0].letter[a1];
+    }printf("\n \n \n");
 
     fclose(fp);
     
@@ -96,18 +107,47 @@ int print_char(int n){
         printf(" ");
     }
     if((n>=2)&&(n<=27)){
-        printf("%c",n+63);
+        printf("%c",n+95);
     }
-    if((n>=28)&&(n<=53)){
-        printf("%c",n+69);
+    if(n==28){
+        printf("%c%c",-61,-96);
     }
-    if((n>=54)&&(n<=117)){
-        printf("%c%c",-61,n-182);  
+    if(n==29){
+        printf("%c%c",-61,-89);  
     }
-    if((n==118)||(n==119)){
-      printf("%c%c",-59,n-228);  
+    if(n==30){
+        printf("%c%c",-61,-88);  
     }
-    if(n==120){
+    if(n==31){
+        printf("%c%c",-61,-87);  
+    }
+    if(n==32){
+        printf("%c%c",-61,-86);  
+    }
+    if(n==33){
+        printf("%c%c",-61,-76);  
+    }
+    if(n==34){
+        printf("%c%c",-61,-94);  
+    }
+    if(n==35){
+        printf("%c%c",-61,-85);  
+    }
+    if(n==36){
+        printf("%c%c",-61,-82);  
+    }
+    if(n==37){
+        printf("%c%c",-61,-81);  
+    }
+    if(n==38){
+        printf("%c%c",-61,-71);  
+    }
+    if(n==39){
+        printf("%c%c",-61,-69);  
+    }
+    if(n==40){
+      printf("%c%c",-59,-109);  
+    }
+    if(n==41){
         printf("%c%c%c",-30,128,153);  
-    }
-}
+    }}
